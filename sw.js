8d723bef10a5af1cdf7a39557b9b9af37bd094bb
グエン ヴアン　ドウツク <097664@leoc.jp>
2026-02-11 11:52:26 +0900

    Add Inter, easing spin, suspense delay, 60ms tick sound

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fc26-random-v1";
+const CACHE = "fc26-random-local-v2";
 const CORE = [
     "./",
     "./index.html",
@@ -6,27 +6,48 @@ const CORE = [
     "./sw.js",
     "./icons/icon-180.png",
     "./icons/icon-192.png",
-    "./icons/icon-512.png"
+    "./icons/icon-512.png",
+    "./sounds/tick.mp3",
+    // logos
+    "./logos/real-madrid.png",
+    "./logos/barcelona.png",
+    "./logos/psg.png",
+    "./logos/liverpool.png",
+    "./logos/man-city.png",
+    "./logos/arsenal.png",
+    "./logos/bayern.png",
+    "./logos/atletico.png",
+    "./logos/newcastle.png",
+    "./logos/napoli.png",
+    "./logos/tottenham.png",
+    "./logos/chelsea.png",
+    "./logos/man-united.png",
+    "./logos/leverkusen.png",
+    "./logos/spain.png",
+    "./logos/france.png",
+    "./logos/argentina.png",
+    "./logos/england.png",
+    "./logos/portugal.png",
+    "./logos/germany.png",
+    "./logos/inter.png"
 ];
 
-self.addEventListener("install", e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+self.addEventListener("install", (e) => {
+    e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)));
     self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
+self.addEventListener("activate", (e) => {
     e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+        caches.keys().then((keys) =>
+            Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
         )
     );
     self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-    if (e.request.url.startsWith(self.location.origin)) {
-        e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-    } else {
-        e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-    }
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((r) => r || fetch(e.request))
+    );
 });
